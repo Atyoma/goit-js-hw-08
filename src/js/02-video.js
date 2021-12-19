eed2be0c09aee50d5bc4f8046d5155d015d1b26f
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2021-12-19 19:39:07 +0200

    add task2 and fixed task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,66 +1,26 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const options = {
-  id: 59777392,
-  width: 640,
-  loop: true,
-};
-
-const play = {
-  duration: 61.857,
-  percent: 0,
-  seconds: 0,
-};
-
-const playing = {
-  duration: 61.857,
-  percent: 0,
-  seconds: 0,
-};
-
-const timeUpdate = {
-  duration: 61.857,
-  percent: 0.049,
-  seconds: 3.034,
-};
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-const onPlay = function ({ duration, percent, seconds }) {
-  //   // data is an object containing properties specific to that event
-};
-
-player.on('play', onPlay);
-
 const LOCALSTORAGE_KEY_VIMEO = 'videoplayer-current-time';
 
-player.on('timeupdate', function (timeUpdate) {
-  // data is an object containing properties specific to that event
-  localStorage.setItem(LOCALSTORAGE_KEY_VIMEO, 'timeupdate');
-  console.log('timeupdate');
-});
+// document.addEventListener('play', videoStart);
 
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+const onGetCurrentTime = function () {
+  player.getCurrentTime().then(sec => {
+    console.log(sec);
 
-      default:
-        // some other error occurred
-        break;
-    }
+    localStorage.setItem(LOCALSTORAGE_KEY_VIMEO, JSON.stringify(sec));
   });
+};
+
+player.on('timeupdate', throttle(onGetCurrentTime, 1000));
+
+const timeToStart = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY_VIMEO)) || 0;
+console.log(timeToStart);
+player.setCurrentTime(timeToStart);
+
+function videoStart(timeToStart) {
+  console.log(timeToStart);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,3 @@
-// import throttle from 'lodash.throttle';
-
-// const formRef = document.querySelector('.feedback-form');
-// const inputRef = document.querySelector('.feedback-form input');
-// const textareaRef = document.querySelector('.feedback-form textarea');
-
-// const LOCALSTORAGE_KEY = 'feedback-form-state';
-
-// const formData = {};
-
-// formRef.addEventListener('submit', saveMessage);
-// formRef.addEventListener('input', throttle(onTextareaInput, 500));
-
-// leavesData();
-
-// function onTextareaInput(e) {
-//   formData[e.target.name] = e.target.value;
-
-//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-//   console.log(formData);
-// }
-
-// function leavesData() {
-//   const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
-//   console.log(savedMessage);
-//   JSON.parse(savedMessage);
-//   if (savedMessage) {
-//     console.log(savedMessage);
-//     // console.log(Object.values(savedMessage));
-//     savedMessage.email = inputRef.value;
-//     savedMessage.message = textareaRef.value;
-//   }
-// }
-
-// function saveMessage(evt) {
-//   evt.preventDefault();
-//   formRef.reset();
-//   localStorage.removeItem(LOCALSTORAGE_KEY);
-//   console.log('отправлено');
-// }
-
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
@@ -58,7 +17,7 @@ function onFormInput(e) {
   formData[e.target.name] = e.target.value;
 
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
+  //   console.log(formData);
 }
 
 function leavesData() {
@@ -75,5 +34,5 @@ function saveData(evt) {
   evt.preventDefault();
   formRef.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log('отправлено');
+  console.log('Oтправлено:', formData);
 }
